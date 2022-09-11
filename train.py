ed126af7405cf2cf445441e32a469b96fd81eb27
IvanMiak <113133149+IvanMiak@users.noreply.github.com>
2022-09-11 18:42:21 +0300

    Update train.py

--- train.py
+++ train.py
@@ -2,10 +2,10 @@ with open(inputFileName, 'r') as inputFile, open(outputFileName, 'w') as outputF
     c = inputFile.readline()
     while c:
         for i in range(len(c)):
-            if 97 <= ord(c[i]) <= 122:
+            if 1072 <= ord(c[i]) <= 1103 or 97 <= ord(c[i]) <= 122:
                 word += c[i]
                 f = 1
-            elif 65 <= ord(c[i]) <= 90:
+            elif 1040 <= ord(c[i]) <= 1071 or 65 <= ord(c[i]) <= 90:
                 word += chr(ord(c[i]) + 32)
                 f = 1
             elif f == 1:
